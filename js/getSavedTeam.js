5a77e50ffa0bc3879243eaeb37a7e4d344dcb554
FirmanJabar <firmanabduljabar@gmail.com>
2020-06-26 00:51:36 +0800

    :zap: edit team favorite page

--- js/getSavedTeam.js
+++ js/getSavedTeam.js
@@ -1,48 +1,48 @@
 function getSavedTeamHTML(teams) {
   let teamHTML = "";
   teams.forEach(function (data) {
-
     let logoTeam = data.crestUrl;
-    if (logoTeam == null || logoTeam == '') {
-      logoTeam = 'img/liga/404.png';
+    if (logoTeam == null || logoTeam == "") {
+      logoTeam = "img/liga/404.png";
     } else {
-      logoTeam = logoTeam.replace(/^http:\/\//i, 'https://');
+      logoTeam = logoTeam.replace(/^http:\/\//i, "https://");
     }
 
     teamHTML += `
-        <div class="row" >
-          <div class="col m3 l4"></div>
-            <div class="col s12 m6 l4">
-            <blockquote class="black-text">Click logo to see details</blockquote>
-              <div class="card grey darken-4">
-                <div class="row">
-                  <a href="./team.html?id=${data.id}">
-                    <div class="col s2"></div>
-                      <div class="col s8 card-image waves-effect waves-block waves-light">
-                        <img alt="${data.name}" src="${logoTeam}" class="responsive-img" style="height:100%"/>
-                      </div>
-                    <div class="col s2"></div>
-                  </a>
-                </div>
+    <div class="container">
+    <div class="col s12">
+      <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row">
+          <div class="col s12 m6 center">
+            <a href="./team.html?id=${data.id}">
+              <img alt="${data.name}" src="${logoTeam}" class="responsive-img">
+            </a>
+          </div>
+          <div class="col s12 m6">
+            <span class="black-text">
+              <div class="hide-on-small-only">
                 <hr>
-                <div class="card-content">
-                  <span class="truncate"><b>${data.name} (${data.tla})</b></span>
-                  <a href="${data.website}" target="_blank">  
-                    <p>${data.website}</p>
-                  </a>
-                  <hr>
-                  <p>Berdiri: ${data.founded}</p>
-                  <p>Stadion: ${data.venue}</p>
-                  <p>Warna: ${data.clubColors}</p>
-                  <p>E-mail: ${data.email}</p>
-                  <p>Telepon: ${data.phone}</p>
-                  <p>Alamat: ${data.address}</p>
-                </div>
+                <span class="truncate"><b>${data.name} (${data.tla})</b></span>
+                <a href="${data.website}" target="_blank">  
+                  <p class="truncate">${data.website}</p>
+                </a>
+                <hr>
+                <p><i class="tiny material-icons">account_balance</i> ${data.founded}</p>
+                <p><i class="tiny material-icons">flag</i> ${data.venue}</p>
+                <p><i class="tiny material-icons">person</i> ${data.clubColors}</p>
+                <p><i class="tiny material-icons">location_on</i> ${data.address}</p>
+                <p><i class="tiny material-icons">phone</i> ${data.phone}</p>
+                <a style="margin-top:10px;" href="./team.html?id=${data.id}" class="cyan darken-4 waves-effect waves-light btn">
+                  <i class="material-icons left">details</i>Detail
+                </a>
               </div>
-            </div>
-          <div class="col m3 l4"></div>
+            </span>
+          </div>
         </div>
-        `;
+      </div>
+    </div>
+    </div>
+    `;
   });
   // Sisipkan komponen card ke dalam elemen dengan id #content
   document.getElementById("team-fav").innerHTML = teamHTML;
--- js/script.js
+++ js/script.js
@@ -1,75 +1,75 @@
-document.addEventListener('DOMContentLoaded', function () {
-	// SIDEBAR NAVIGATION
-	var sidenav = document.querySelectorAll('.sidenav');
-	M.Sidenav.init(sidenav);
-	loadNav();
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    loadNav();
 
-	// PARALLAX
-	var parallax = document.querySelectorAll('.parallax');
-	M.Parallax.init(parallax);
+    // PARALLAX
+    var parallax = document.querySelectorAll(".parallax");
+    M.Parallax.init(parallax);
 
-	function loadNav() {
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function () {
-			if (this.readyState == 4) {
-				if (this.status != 200) return;
+    function loadNav() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status != 200) return;
 
-				// Muat daftar tautan menu
-				document.querySelectorAll(".topnav, .sidenav")
-					.forEach(function (elm) {
-						elm.innerHTML = xhttp.responseText;
-					});
+          // Muat daftar tautan menu
+          const text = document.querySelectorAll(".topnav, .botnav");
+          text.forEach(function (elm) {
+            elm.innerHTML = xhttp.responseText;
+          });
 
-				// Daftarkan event listener untuk setiap tautan menu
-				document.querySelectorAll('.sidenav a, .topnav a')
-					.forEach(function (elm) {
-						elm.addEventListener('click', function (event) {
-							// Tutup sidenav
-							var sidenav = document.querySelector('.sidenav');
-							M.Sidenav.getInstance(sidenav).close();
+          // Daftarkan event listener untuk setiap tautan menu
+          const link = document.querySelectorAll(".botnav li a, .topnav li a");
+          link.forEach(function (elm) {
+            elm.addEventListener("click", function (event) {
+              // Muat konten halaman yang dipanggil
+              if (event.target.attributes.href) {
+                page = event.target.attributes.href.value.substr(1);
+                loadPage(page);
+              } else {
+                page2 = event.target.parentNode.attributes.href.value.substr(1);
+                loadPage(page2);
+              }
+            });
+          });
+        }
+      };
+      xhttp.open("GET", "nav.html", true);
+      xhttp.send();
+    }
 
-							// Muat konten halaman yang dipanggil 
-							page = event.target.getAttribute('href').substr(1);
-							loadPage(page);
-						});
-					});
-			}
-		};
-		xhttp.open("GET", 'nav.html', true);
-		xhttp.send();
-	}
+    // Load page content
+    var page = window.location.hash.substr(1);
+    if (page == "") page = "home";
+    loadPage(page);
 
-	// Load page content
-	var page = window.location.hash.substr(1);
-	if (page == '') page = 'home';
-	loadPage(page);
-
-	function loadPage(page) {
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function () {
-			if (this.readyState == 4) {
-				var content = document.querySelector(".body-content");
-				if (this.status == 200) {
-					content.innerHTML = xhttp.responseText;
-					if (page == "home") {
-						// content.append = eval($('.parallax').parallax());
-						var parallax = document.querySelectorAll('.parallax');
-						M.Parallax.init(parallax);
-						getCompetitions();
-					} else if (page == "saved") {
-						getSavedTeam();
-					}
-				} else if (this.status == 404) {
-					content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
-				} else {
-					content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
-				}
-			}
-		};
-		xhttp.open("GET", 'pages/' + page + '.html', true);
-		xhttp.send();
-	}
-
-}, {
-	passive: true
-});
+    function loadPage(page) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          var content = document.querySelector(".body-content");
+          if (this.status == 200) {
+            content.innerHTML = xhttp.responseText;
+            if (page == "home") {
+              var parallax = document.querySelectorAll(".parallax");
+              M.Parallax.init(parallax);
+              getCompetitions();
+            } else if (page == "saved") {
+              getSavedTeam();
+            }
+          } else if (this.status == 404) {
+            content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+          } else {
+            content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
+          }
+        }
+      };
+      xhttp.open("GET", "pages/" + page + ".html", true);
+      xhttp.send();
+    }
+  },
+  {
+    passive: true,
+  }
+);
