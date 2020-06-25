23ae4ff7e7732732ed8baa2c41b62cde2b6b9c8a
FirmanJabar <firmanabduljabar@gmail.com>
2020-06-25 09:15:07 +0800

    fix img logo league

--- js/getCompetitions.js
+++ js/getCompetitions.js
@@ -2,21 +2,23 @@ function getCompetitionsHTML(data) {
   let leagueHTML = "";
   data.competitions.forEach(function (liga) {
     leagueHTML += `
-      <div class="col s12 m6 l4" >
+      <div class="col s6 m4" >
       <a href="./standing.html?id=${liga.id}">
         <div class="card">
           <div class="card-image waves-effect waves-block waves-light">
             <img alt="${liga.area.name}" src="img/liga/${liga.code}.png" style="width:100%"/>
           </div>
-          <hr>
-          <div class="card-content">
+          <p class="garis"></p>
+          <div class="card-content" style="margin-top:-25px;">
             <span class="truncate"><b>${liga.name}</b></span>
             <p>${liga.area.name}</p>
-            <hr>
-            <p>Kode Liga: ${liga.code}</p>
-            <p>Mulai: ${liga.currentSeason.startDate}</p>
-            <p>Berakhir: ${liga.currentSeason.endDate}</p>
-            <p>Pertandingan: ${liga.currentSeason.currentMatchday}</p>
+            <span class="sm-gone">
+              <p class="garis" style="margin:10px 0"></p>
+              <p>Kode Liga: ${liga.code}</p>
+              <p>Mulai: ${liga.currentSeason.startDate}</p>
+              <p>Berakhir: ${liga.currentSeason.endDate}</p>
+              <p>Pertandingan: ${liga.currentSeason.currentMatchday}</p>
+            </span>
           </div>
         </div>
       </a>
