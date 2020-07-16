4f53c07313550709ebb7250277723a65b5316b7d
FirmanJabar <firmanabduljabar@gmail.com>
2020-07-16 22:53:31 +0800

    :heavy_minus_sign: delete node_modules

--- push.js
+++ push.js
@@ -1,31 +1,29 @@
-var webPush = require('web-push');
+var webPush = require("web-push");
 
 const vapidKeys = {
-    "publicKey": "BIAdhZ7nFOum1Qg5y3sYs8nA_BeCed6mxPQQvdWDGAqpdWV6oqAESl8ZHe7gPG_c4a5qNm7PEc1fvrsX_uxc2mc",
-    "privateKey": "XCHTY05rasJG0qvgBDGcBLtCAJZt4NjGSorKZHERRq4"
+  publicKey:
+    "BIAdhZ7nFOum1Qg5y3sYs8nA_BeCed6mxPQQvdWDGAqpdWV6oqAESl8ZHe7gPG_c4a5qNm7PEc1fvrsX_uxc2mc",
+  privateKey: "XCHTY05rasJG0qvgBDGcBLtCAJZt4NjGSorKZHERRq4",
 };
-
 
 webPush.setVapidDetails(
-    'mailto:example@yourdomain.org',
-    vapidKeys.publicKey,
-    vapidKeys.privateKey
-)
+  "mailto:example@yourdomain.org",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
 var pushSubscription = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/deIFWyWkAq4:APA91bFHTOvT7vmho3CbwT6AFHDZs6ut0zAioyWOrm9iGJgqfVylI5WWU6TVyaR1A5WbLO3UmvS7bILzDKP8eZXtwJfruDgIr4qxR6Z5etaZ4-FSjjImiXKoS5xBAsCuoOa6RazuFqij",
-    "keys": {
-        "p256dh": "BOzDqzg95TjVtl31m/xNT/dFFYBLJfMpjHzK/7qv6iiFAB57DXS3/CUDJWg9pj/+LB2dGiwwmBlmJ8NT9QyEPmg=",
-        "auth": "p2F+3aLiWvZ2IEW+YvCwZA=="
-    }
+  endpoint:
+    "https://sg2p.notify.windows.com/w/?token=BQYAAAC62QWvrS8xrl%2fGU1VF4i0H2Etg2VgsdSfopfTQZEIyIsRJv%2fKZi8503DedrOddbn20AfPDLnn1p1TgTcczwpzrASCeGR%2byYijHDtDWbrPXhaeDbyAXd8Kk4NNFyP1dhNZ3r8X3dmfefepfwfixVWxlZ2IyyS6bDfX0PNmE40lR0DPrQTBAp9ATIM8J3DNGZXuw4cTxMdii9VzbrvOdgUyCppkKaMh%2bd%2bTtWphaD4vEYGVfeavbzYwjLFVRRar4fqcN2N0Z5DUf%2fMXULwmmP5iEMvdDm54yfdJZXJ%2bRVIJCwHUSdF9f8S0Eb81LzLpBUzsZ1J8rMTXnDI8b%2fkoq3GsN",
+  keys: {
+    p256dh:
+      "BBmSyk+dSpdDSbSMW2Tkk1a4qt6BDPvcRoz+pZHqOH4HGpF+FIwQQnCtFVMtulcGKvT8fc0TsCwfexrcd632kI4=",
+    auth: "cEp43fedaNtuURv6cbvNCA==",
+  },
 };
-var payload = 'Selamat! Aplikasi Anda sudah dapat menerima push notifikasi!';
+var payload = "Selamat! Aplikasi Anda sudah dapat menerima push notifikasi!";
 
 var options = {
-    gcmAPIKey: '988799844009',
-    TTL: 60
+  gcmAPIKey: "988799844009",
+  TTL: 60,
 };
-webPush.sendNotification(
-    pushSubscription,
-    payload,
-    options
-);
+webPush.sendNotification(pushSubscription, payload, options);
